40a392d85d8af5dac05289e1a8fb38558ec4be48
Julien-MANSOUR <julienmansour@gmail.com>
2021-02-26 19:36:43 +0100

    Creating Toolbar Widget

--- mainn.py
+++ mainn.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 
 
 class Main(QMainWindow):
@@ -9,17 +10,33 @@ class Main(QMainWindow):
         self.setWindowTitle("Product Manager")
         self.setWindowIcon(QIcon("icons/icon.ico"))
         self.setGeometry(450, 150, 1350, 750)
-        self.setFixedSize(self.size())# to fix windows size without minimize or maximize it 
+        self.setFixedSize(self.size())  # to fix windows size without minimize or maximize it
         self.UI()
         self.show()
 
     def UI(self):
-        pass
+        self.toolBar()
 
+    def toolBar(self):
+        self.tb = self.addToolBar("Tool Bar")
+        self.tb.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)  # so the name we chose could appear under the button
+        ##################ToolBar Button###################
+        #################Add Product ######################
+        self.addProduct=QAction(QIcon("icons/add.png"),"Add Product",self)
+        self.tb.addAction(self.addProduct)
+        self.tb.addSeparator()
+        #################Add Product ######################
+        self.addMember=QAction(QIcon("icons/users.png"),"Add Member",self)
+        self.tb.addAction(self.addMember)
+        self.tb.addSeparator()
+        #################Add Product ######################
+        self.sellProduct=QAction(QIcon("icons/sell.png"),"Sell Product",self)
+        self.tb.addAction(self.sellProduct)
+        self.tb.addSeparator()
 
 def main():
     App = QApplication(sys.argv)
-    #App.setWindowIcon(QIcon("icons/icon.ico")) we con put an icon to the window in this way too
+    # App.setWindowIcon(QIcon("icons/icon.ico")) we con put an icon to the window in this way too
     window = Main()
     sys.exit(App.exec_())
 
