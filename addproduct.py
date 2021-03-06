1310b93e85626ed8686811571d6b88b46207a08e
Julien-MANSOUR <julienmansour@gmail.com>
2021-03-06 14:26:55 +0100

    Creating widgets and layouts for addproduct window

--- addproduct.py
+++ addproduct.py
@@ -20,7 +20,57 @@ class AddProduct(QWidget):
         self.show()
 
     def UI(self):
-        pass
+        self.widgets()
+        self.layouts()
+
+
+    def widgets(self):
+        ###############widgets of top layout#################
+        self.addProductImg=QLabel()
+        self.img=QPixmap("icons/addproduct.png")
+        self.addProductImg.setPixmap(self.img)
+        self.titleText=QLabel("Add Product")
+        ###############widgets of bottom layout#################
+        self.nameEntry=QLineEdit()
+        self.nameEntry.setPlaceholderText("Enter name of product")
+        self.manufactirerEntry=QLineEdit()
+        self.manufactirerEntry.setPlaceholderText("Enter name of manufacturer")
+        self.priceEntry=QLineEdit()
+        self.priceEntry.setPlaceholderText("Enter price of product")
+        self.qoutaEntry=QLineEdit()
+        self.qoutaEntry.setPlaceholderText("Enter qouta of product")
+        self.uploadBtn=QPushButton("Upload")
+        self.submitBtn=QPushButton("Submit")
+
+
+
+
+
+    def layouts(self):
+        self.mainLayout=QVBoxLayout()
+        self.topLayout=QHBoxLayout()
+        self.bottomLayout=QFormLayout()
+        self.topFrame=QFrame() #QFrame is similar to groupeBox
+        self.bottomFrame=QFrame()
+        ################# add widgets######################
+
+        #################top layout widgets################
+        self.topLayout.addWidget(self.addProductImg)
+        self.topLayout.addWidget(self.titleText)
+        self.topFrame.setLayout(self.topLayout)
+        #################bottom layout widgets################
+        self.bottomLayout.addRow(QLabel("Name: "),self.nameEntry)
+        self.bottomLayout.addRow(QLabel("Manufacturer: "),self.manufactirerEntry)
+        self.bottomLayout.addRow(QLabel("Price: "),self.priceEntry)
+        self.bottomLayout.addRow(QLabel("Qouta: "),self.qoutaEntry)
+        self.bottomLayout.addRow(QLabel("Upload: "),self.uploadBtn)
+        self.bottomLayout.addRow(QLabel(""),self.submitBtn)
+        self.bottomFrame.setLayout(self.bottomLayout)
+
+        ####################main layout widgets#################3
+        self.mainLayout.addWidget(self.topFrame)
+        self.mainLayout.addWidget(self.bottomFrame)
+        self.setLayout(self.mainLayout)
 
 
 def main():
