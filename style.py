01853bc413c08635ceac7e4ad4b5b3defc55dd43
Julien-MANSOUR <julienmansour@gmail.com>
2021-03-13 18:26:36 +0100

    Creating style functions for search & list bars

---
+++ style.py
@@ -0,0 +1,24 @@
+def searchBoxStyle():
+    return"""
+    QGroupBox{
+    background-color: #9bc9ff;
+    font:15pt Times Bold;
+    color:white;
+    border:2px solid gray;
+    border-radius:15px;
+ 
+    }
+    """
+
+def listhBoxStyle():
+    return"""
+    QGroupBox{
+    background-color: #fcc324;
+    font: 15pt Times Bold;
+    color: white;
+    border:2px solid gray;
+    border-radius: 15px
+    
+    }
+    
+    """
